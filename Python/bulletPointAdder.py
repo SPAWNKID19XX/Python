77b27fd241b7b8287961fe83d3c1e260ed6143ff
Boris Isac <borisisac@gmail.com>
2022-10-02 00:00:08 +0100

    had been added some exercises

--- Python/bulletPointAdder.py
+++ Python/bulletPointAdder.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 #python 3
 #put * before animal names and copy all lines to clipboard
-from functools import update_wrapper
 import pyperclip
 
 #Original String Line
@@ -21,28 +19,4 @@ for i in range(len(arr)):
 print('changed text with stars before lists.'.upper())
 text = '\n'.join(arr)
 pyperclip.copy(text)
-=======
-#python 3
-#put * before animal names and copy all lines to clipboard
-from functools import update_wrapper
-import pyperclip
-
-#Original String Line
-print('ORIINAL TEXT')
-text = 'Animals List \nFishes List \nBiologist List \nPlants List'
-print(text)
-pyperclip.copy(text) #!copy a text in clipboard
-
-#record Original String in array ARR
-#separator '\n' post with argument to method split
-arr = text.split('\n')
-for i in range(len(arr)):
-    arr[i] = '* ' + arr[i]
-
-#to Make: Devide a lines, post a stars(*) before each line
-#changed text with stars before lists
-print('changed text with stars before lists.'.upper())
-text = '\n'.join(arr)
-pyperclip.copy(text)
->>>>>>> 2e949e990693a6f7fd6e5331c39ade638c4d7d45
 print(text)
--- Python/codewarExercises/expressNumberAsSumOfFourSquares.py
+++ Python/codewarExercises/expressNumberAsSumOfFourSquares.py
@@ -1,24 +1,9 @@
-from typing import Tuple
+from math import sqrt
 import random
+n= random.randint(2**10, 2**15)
 
-#assign a random value to randNum
-randNum = random.randint(100, 200)
-
-#function wich will check and print a maxim 
-#sqr(value) before randNum 
-
-def four_squares(n: int) -> Tuple[int, int, int, int]:
-    res = {}
-    for a in range(10):
-        for b in range(10):
-            for c in range(10):
-                for d in range(10):
-                    #print(a**2+b**2+c**2+d**2)
-                    if a**2+b**2+c**2+d**2 == n:
-                        lol = [a, b, c, d]
-                        print(lol)
-                        if lol not in res:
-                            res.append(lol)
-    return res
-                    
-print(four_squares(randNum))
+a = int(sqrt(n))
+b = int(sqrt(n-a**2))
+c = int(sqrt(n-a**2-b**2))
+d = int(sqrt(n-a**2-b**2-c**2))
+print(a, b, c, d, n)
