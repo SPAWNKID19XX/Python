0868d11b026ac2ab44b570814cca7af003986e98
Boris <borisisac@gmail.com>
2023-04-20 04:20:18 +0100

    added task manager using mysql, created backand for progectManager frontEnd using sqlite3

--- productManager/app.py
+++ productManager/app.py
@@ -1,82 +1,166 @@
-import tkinter
-from tkinter import *
 from tkinter import ttk
-import mysql.connector  # import mysql module
+from tkinter import *
+import  sqlite3
 
-productList = [{'name':'Impressora 3D','price':1000},{'name':'Arduino','price':20},{'name':'Raspberry Pi 4','price':50}]
+class Product():
+    db = 'database/product.db'
+
+    def db_consulta(self, consulta, parametros = ()):
+        with sqlite3.connect(self.db) as connection:
+            cursor = connection.cursor()
+            resultado = cursor.execute(consulta, parametros)
+            connection.commit()
+        return resultado
+
+    def get_all_products(self):
+        registos_tabela = self.table.get_children()
+        for linha in registos_tabela:
+            self.table.delete(linha)
+
+        query = "SELECT * FROM product ORDER BY name DESC"
+        registos = self.db_consulta(query)
+        for linha in registos:
+            print(linha)
+            self.table.insert('', 0, text = linha[1], values = linha[2])
 
 
-class Product:
+    def validation_name(self):
+        insertedName = self.name.get()
+        return len(insertedName) != 0
+    def validation_price(self):
+        insertedPrice = self.price.get()
+        return len(insertedPrice) != 0
+
+    def add_product(self):
+        if self.validation_price() and self.validation_name():
+            query = 'INSERT INTO product VALUES(NULL,?,?)'
+            parametros = (self.name.get(), self.price.get())
+            self.db_consulta(query, parametros)
+            print(self.name.get())
+            print(self.price.get())
+            self.msgLabel['text'] = 'Product {} has been added'.format(self.name.get())
+            self.name.delete(0, END)
+            self.price.delete(0, END)
+        elif self.validation_price() == False and self.validation_name():
+            self.msgLabel['text'] = 'Price must be enterred'
+        elif self.validation_price() and self.validation_name() == False:
+            self.msgLabel['text'] = 'Name must be enterred'
+        else:
+            self.msgLabel['text'] = 'Name and Price must be enterred'
+        self.get_all_products()
+
+    def delProd(self):
+        self.msgLabel['text'] = ''
+        try:
+            print(self.table.item(self.table.selection())['text'][0])
+        except IndexError as erro:
+            self.msgLabel['text'] = 'You should select some item'
+            return
+        
+        self.msgLabel['text'] = ''
+        selectItem = self.table.item(self.table.selection())['text']
+        query = "DELETE FROM product WHERE name = ?"
+        self.db_consulta(query, (selectItem,))
+        self.msgLabel['text'] = '{} has been deleted'.format(selectItem)
+        self.get_all_products()
+        
+
+    def editProd(self):
+        self.msgLabel['text'] = ''
+        try:
+            print(self.table.item(self.table.selection())['text'][0])
+        except IndexError as erro:
+            self.msgLabel['text'] = 'You should select some item'
+            return
+        
+        selectItem = self.table.item(self.table.selection())['text']
+        oldPrice = self.table.item(self.table.selection())['values'][0]
+        
+        self.editWindow = Toplevel()
+        self.editWindow.title('Edit product')
+        self.editWindow.resizable(1,1)
+        self.editWindow.wm_iconbitmap('template/icon.ico')
+        self.editWindow.geometry('400x500')
+        
+        titleEditWindow = Label(self.editWindow, text='Edit Product', font=('calibri', 50, 'bold'))
+        titleEditWindow.grid(row=0, column=0)
+        
+        frameEd = LabelFrame(self.editWindow, text='Edit Product')
+        frameEd.grid(row=1, column=0, columnspan=20,pady=20)
+        
+        self.oldNameLbl = Label(frameEd, text='Old Name: ')
+        self.oldNameLbl.grid(row=2, column=0)
+        self.oldNameLblValue = Entry(frameEd, textvariable=StringVar(self.editWindow, value=selectItem), state='readonly')
+        self.oldNameLblValue.grid(row=2, column=1)
+        
+        self.newNameLbl = Label(frameEd, text='New Name: ')
+        self.newNameLbl.grid(row=3, column=0)
+        self.newNameInput = Entry(frameEd)
+        self.newNameInput.grid(row=3, column=1)
+        
+        self.oldPriceLbl = Label(frameEd, text='Old Price: ')
+        self.oldPriceLbl.grid(row=4, column=0)
+        self.oldPriceLblValue = Entry(frameEd, textvariable=StringVar(self.editWindow, value=oldPrice), state='readonly')
+        self.oldPriceLblValue.grid(row=4, column=1)
+        
+        self.newPriceLbl = Label(frameEd, text='New Price: ')
+        self.newPriceLbl.grid(row=5, column=0)
+        self.newPriceInput = Entry(frameEd)
+        self.newPriceInput.grid(row=5, column=1)
+        
+        
+        self.editButton = ttk.Button(frameEd, text='Edit Product')
+        self.editButton.grid(row=6, columnspan=2, sticky=W+E)
+        
+        
+    
     def __init__(self, root):
         self.window = root
-        self.window.geometry('1300x600+100+50')
-        self.window.update_idletasks()
-        self.window.resizable(True,True)  # Ativar a redimensionamento da janela. Para desativá-la: (0,0)
-        self.window.wm_iconbitmap('template/M6_p2_icon.ico')
-        self.window.title('App Product Manager')
-        frame = LabelFrame(self.window, text = 'Registr new Product')
-        frame.grid(row=3, column=3, columnspan= 2, pady=20)
-        # Label Nome
-        self.nameLabel = Label(frame, text="Name: ")# Label belongs to frame
-        self.nameLabel.grid(row=1, column=0)# Possition
-        # input Name
-        self.name = Entry(frame)# input belongs to frame
-        self.name.focus()# Para que o foco do rato vá a esta Entry no início
-        self.name.grid(row=1, column=1) #Possition
-        # Label Price
-        self.priceLabel = Label(frame, text='Price') # Labal belongs to frame
-        self.priceLabel.grid(row=2, column=1)# Possition
-        # Input Price
-        self.price = Entry(frame)# input belongs to frame
-        self.price.grid(row=2, column=1)# possition
-        #Button SAVE
-        self.buttonSave = Button(frame,text='Save', width=20)
-        self.buttonSave.grid(row=1, column=2, padx=20)
+        self.window.title("App Product Manager") # Título da janela
+        self.window.resizable(1,1) # Ativar a redimensionamento da janela. Para desativá-la: (0,0)
+        self.window.wm_iconbitmap('template/icon.ico')
 
-        # Tabela de Produtos
-        # Estilo personalizado para a tabela
+        frame = LabelFrame(self.window, text="Add New Product")
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20, padx=20)
+
+        self.lName = Label(frame, text='Name')
+        self.lName.grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.focus()
+        self.name.grid(row=1, column=1)
+
+        self.lPrice = Label(frame, text='Price')
+        self.lPrice.grid(row=2, column=0)
+        self.price = Entry(frame)
+        self.price.focus()
+        self.price.grid(row=2, column=1)
+
+        self.msgLabel = Label(text='', fg='red')
+        self.msgLabel.grid(row = 3,column = 0, columnspan = 2, sticky= W + E)
+
+        self.buttonAdd = ttk.Button(frame, text='Add Product', command=self.add_product)
+        self.buttonAdd.grid(row=3, columnspan=2, sticky=W+E)
+
         style = ttk.Style()
-        style.configure("mystyle.Treeview", highlightthickness=0, bd=0, font=('Calibri', 11))
-        # Modifica-se a fonte da tabela
-        style.configure("mystyle.Treeview.Heading", font=('Calibri', 13, 'bold'))
-        # Modifica-se a fonte das cabeceiras
-        style.layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})])
-        # Eliminar as bordas
-        # Estrutura da tabela
-        self.tabela = ttk.Treeview(height=20, columns=2, style="mystyle.Treeview")
-        self.tabela.grid(row=4, column=3, columnspan=2)
-        self.tabela.heading('#0', text='Nome', anchor=CENTER)  # Cabeçalho 0
-        self.tabela.heading('#1', text='Preço', anchor=CENTER)  # Cabeçalho 1
+        style.configure("mystyle.Treeview", highlightthickness=0, bd=0, font=('Calibri',11))  # Modifica-se a fonte da tabela
+        style.configure("mystyle.Treeview.Heading", font=('Calibri', 13, 'bold')) # Modifica-se a fonte das cabeceiras
+        style.layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})]) # Eliminar as bordas
 
+        self.table = ttk.Treeview(height=20, columns=2, style="mystyle.Treeview")
+        self.table.grid(row=4, column=0, columnspan=2)
+        self.table.heading('#0', text='Name', anchor=CENTER)  # Cabeçalho 0
+        self.table.heading('#1', text='Price', anchor=CENTER)  # Cabeçalho 1
 
-#create Database and table
-connect = mysql.connector.connect(
-    host='127.0.0.1',
-    user = 'Boris',
-    password = 'root'
-)
+        self.get_all_products()
 
-print('Connection:',connect.is_connected())
+        # Botões de Eliminar e Editar
+        botao_eliminar = ttk.Button(text = 'ELIMINAR', command=self.delProd)
+        botao_eliminar.grid(row = 5, column = 0, sticky = W + E)
+        botao_editar = ttk.Button(text='EDITAR', command=self.editProd)
+        botao_editar.grid(row = 5, column = 1, sticky = W + E)
 
-cursor = connect.cursor()
-
-#create DataBase
-query = "CREATE DATABASE if not exists dsktopApp"
-cursor.execute(query)
-#Create table
-query = "USE dsktopApp"
-a = cursor.execute(query)
-query = "CREATE TABLE if not exists products(id int NOT NULL AUTO_INCREMENT,name TEXT NOT NULL,price FLOAT NOT NULL,PRIMARY KEY (id));)"
-cursor.execute(query)
-
-query = 'SELECT * FROM products'
-cursor.execute(query)
-
-
-
-'''
 if __name__ == '__main__':
     root = Tk()
+    root.geometry("400x600")
     app = Product(root)
     root.mainloop()
-'''
